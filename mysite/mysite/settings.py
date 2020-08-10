def0888858a469e70e1c38ae6fd089bdd39f7243
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-08-10 19:57:34 +0530

    "changes"

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -53,6 +53,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -68,6 +84,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
